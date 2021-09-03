df05ecc07d43251bab4f56d9fd0bdea5a906399a
AcePatterson <acepttrsn@gmail.com>
2021-09-03 10:38:00 +0200

    Initial commit

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { proxy } from 'valtio'
+
+export const state = proxy({
+  intro: true,
+  colors: ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934'],
+  decals: ['react', 'three2', 'pmndrs'],
+  color: '#EFBD4E',
+  decal: 'three2'
+})
